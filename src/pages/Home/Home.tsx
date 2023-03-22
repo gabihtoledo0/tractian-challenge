dbf1cf17e223c2f1434f4ac13cc9bf90822293a3
gabihtoledo0 <gabitoledo60@gmail.com>
2023-03-22 17:31:04 -0300

    adjusment

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,16 @@
-import { Layout, Menu } from "antd"
+import { Layout } from "antd"
 import Assets from "../../components/Sections/Assets"
-import { colors } from "../../utils/colors"
-import HeaderComponent from "../../components/Header/Header"
 import CompaniesUnitsAndUsers from "../../components/Sections/CompaniesUnitsAndUsers"
-import "./Home.css"
 import WorkOrders from "../../components/Sections/WorkOrders"
+import HeaderComponent from "../../components/Header/Header"
+import "./Home.css"
 
 function Home() {
   const { Content } = Layout
 
   return (
     <Layout>
-      <HeaderComponent>
-        <Menu
-          style={{ background: "transparent", borderBottom: 0 }}
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-        >
-          <Menu.Item key="1" className="hoverItem">
-            <span style={{ color: colors.textSecondary }}>Ativos</span>
-          </Menu.Item>
-          <Menu.Item key="1" className="hoverItem">
-            <span style={{ color: colors.textSecondary }}>Empresas</span>
-          </Menu.Item>
-          <Menu.Item key="1" className="hoverItem">
-            <span style={{ color: colors.textSecondary }}>Usuários</span>
-          </Menu.Item>
-        </Menu>
-      </HeaderComponent>
-
+      <HeaderComponent />
       <Content>
         <Assets />
         <CompaniesUnitsAndUsers />
